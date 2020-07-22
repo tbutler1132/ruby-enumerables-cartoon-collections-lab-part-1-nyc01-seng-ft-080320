ad8bc58c8057ab379284f1af2dfa71909c3aa239
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-22 16:38:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,8 @@
 def greet_characters(array)
+  array.each do |element|
+    puts "Hello #{element}!"
+  end 
+
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
